798108c7c3e5aeb71ac9fe9ccbef147a82886f8b
ManikSheoran <sheoranmanik@gmail.com>
2026-07-31 11:14:26 +0530

    Add time delta

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    dt = 0.0
     while 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-
+        dt = clock.tick(60) / 1000
     pygame.display.flip()
 
 
